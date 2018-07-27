8bf4866ad2826b26124d9dd93185e3f656d817e7
Alexandr Slobodnichenko <aslobodnichenko@gmail.com>
2018-07-27 23:50:23 +0300

    new task arrays

---
+++ src/prj05/TaskArrays.java
@@ -0,0 +1,15 @@
+package prj05;
+
+public class TaskArrays {
+
+	public static void main(String[] args) {
+		
+		int[] mas = new int[10];
+		for (int i = 1; i<10;i++)
+			mas[i]=i;
+		
+			for(int i= 0;i< mas.length;i++)
+				System.out.println(mas[i]);
+
+			}
+	}
