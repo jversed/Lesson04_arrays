fb10c7038eb319b208096060052030074f43c2be
Alexandr Slobodnichenko <aslobodnichenko@gmail.com>
2018-08-03 20:20:54 +0300

    add some comments

--- src/prj05/TaskArrays.java
+++ src/prj05/TaskArrays.java
@@ -4,12 +4,12 @@ public class TaskArrays {
 
 	public static void main(String[] args) {
 		
-		int[] mas = new int[10];
-		for (int i = 1; i<10;i++)
-			mas[i]=i;
+		int[] mas = new int[10]; // creating new massive named "mas"
+		for (int i = 1; i<10;i++) // going through mas from 0 to 9
+			mas[i]=i; // each next cell getting nember = index
 		
 			for(int i= 0;i< mas.length;i++)
-				System.out.println(mas[i]);
+				System.out.println(mas[i]); // showing on console every cell of array 
 
 			}
 	}
